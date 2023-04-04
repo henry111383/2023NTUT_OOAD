e89a16696bce0fbd3fe57d16cb054aa2a3fe0fb1
JingHan <henry111383@gmail.com>
2023-04-04 21:30:14 +0800

    0.0.1

--- src/buttom_plot.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-def plotxy() :
-    xx = np.arange(start=0, stop=10, step=0.1, dtype=float)
-    yy =  np.power(xx, 4)
-
-    plt.plot(xx, yy)
-    plt.show()
